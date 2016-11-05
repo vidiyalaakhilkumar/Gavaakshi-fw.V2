574e6890c9116da83461bb3783383a9144725063
Muralidhar Reddy Challa <srmurali002@gmail.com>
2016-11-04 23:14:08 -0700

    Add files via upload

---
+++ public/javascripts/app.js
@@ -0,0 +1,112 @@
+(function(){
+	var app = angular.module('projectRtc', [],
+		function($locationProvider){$locationProvider.html5Mode(true);}
+    );
+	var client = new PeerManager();
+	var mediaConfig = {
+        audio:true,
+        video: {
+			mandatory: {},
+			optional: []
+        }
+    };
+
+    app.factory('camera', ['$rootScope', '$window', function($rootScope, $window){
+    	var camera = {};
+    	camera.preview = $window.document.getElementById('localVideo');
+
+    	camera.start = function(){
+			return requestUserMedia(mediaConfig)
+			.then(function(stream){			
+				attachMediaStream(camera.preview, stream);
+				client.setLocalStream(stream);
+				camera.stream = stream;
+				$rootScope.$broadcast('cameraIsOn',true);
+			})
+			.catch(Error('Failed to get access to local media.'));
+		};
+    	camera.stop = function(){
+			return requestUserMedia(mediaConfig)
+			.then(function(stream){		
+				stream = null;
+				attachMediaStream(camera.preview, stream);
+				client.setLocalStream(stream);
+				camera.stream = stream;
+				$rootScope.$broadcast('cameraIsOn',false);
+			})
+			.catch(Error('Cannot terminate stream.'));
+		};
+		return camera;
+    }]);
+
+	app.controller('RemoteStreamsController', ['camera', '$location', '$http', function(camera, $location, $http){
+		var rtc = this;
+		rtc.remoteStreams = [];
+		function getStreamById(id) {
+		    for(var i=0; i<rtc.remoteStreams.length;i++) {
+		    	if (rtc.remoteStreams[i].id === id) {return rtc.remoteStreams[i];}
+		    }
+		}
+		rtc.loadData = function () {
+			// get list of streams from the server
+			$http.get('/streams.json').success(function(data){
+				// filter own stream
+				var streams = data.filter(function(stream) {
+			      	return stream.id != client.getId();
+			    });
+			    // get former state
+			    for(var i=0; i<streams.length;i++) {
+			    	var stream = getStreamById(streams[i].id);
+			    	streams[i].isPlaying = (!!stream) ? stream.isPLaying : false;
+			    }
+			    // save new streams
+			    rtc.remoteStreams = streams;
+			});
+		};
+
+		rtc.view = function(stream){
+			client.peerInit(stream.id);
+			stream.isPlaying = !stream.isPlaying;
+		};
+
+
+		//initial load
+		rtc.loadData();
+    	
+	}]);
+
+	app.controller('LocalStreamController',['camera', '$scope', '$window', function(camera, $scope, $window){
+		var localStream = this;
+		localStream.name = 'enter your number';
+		localStream.link = '';
+		localStream.cameraIsOn = false;
+
+		$scope.$on('cameraIsOn', function(event,data) {
+    		$scope.$apply(function() {
+		    	localStream.cameraIsOn = data;
+		    });
+		});
+
+		localStream.toggleCam = function(){
+			if(localStream.cameraIsOn){
+				camera.stop()
+				.then(function(result){
+					client.send('leave');
+	    			client.setLocalStream(null);
+				})
+				.catch(function(err) {
+					console.log(err);
+				});
+			} else {
+				camera.start()
+				.then(function(result) {
+					localStream.link = $window.location.host + '/' + client.getId();
+					client.send('readyToStream', { name: localStream.name });
+				})
+				.catch(function(err) {
+					console.log(err);
+				});
+			}
+		};
+	}]);
+})();
